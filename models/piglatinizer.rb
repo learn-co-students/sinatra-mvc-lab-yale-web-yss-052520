079bdc86c65e902a7f42482d8fa0bdcfa6cef922
alexisteh <wenhui.teh23@gmail.com>
2020-06-15 20:28:13 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,45 @@
+class PigLatinizer 
+    def initialize 
+    end 
+
+    def piglatinize(input) 
+        word_array = input.split(" ") 
+        word_array.map do |word| 
+            self.piglatinize_word(word)
+        end.join(" ")
+    end 
+
+    def piglatinize_word(text) 
+        front_cons = self.front_cons(text) 
+        front_number = front_cons.length 
+        char_array = text.split("") 
+        char_array.shift(front_number) 
+        char_array << front_cons.join("") 
+        if front_number == 0 
+            char_array << "way"
+        else
+            char_array << "ay" 
+        end
+        char_array.join("") 
+    end 
+
+    def front_cons(text) 
+        char_array = text.split("")
+        front_cons = [] 
+        char_array.each do |letter|
+            if self.vowel?(letter) 
+                return front_cons 
+            else 
+                front_cons << letter 
+            end 
+        end 
+        front_cons
+    end 
+
+    def vowel?(letter)
+        if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U"   
+            return true 
+        end 
+        return false 
+    end 
+end 
