dad7aa12ff0091cb4d95b4b92a505c69e56de0ca
justin_james <jjames11799@gmail.com>
2020-06-18 07:20:21 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    set :views, "views"
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do 
+        @prompt = PigLatinizer.new
+        @latinized = @prompt.piglatinize(params[:user_phrase])
+       
+        erb :new 
+    end
+
+    
+
 end
