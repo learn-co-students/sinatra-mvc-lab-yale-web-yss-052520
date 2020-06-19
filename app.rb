d8240d2d47b65975b96411e06a82db7371f38b6a
chad <chad.palmer@yale.edu>
2020-06-19 12:45:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @translated = PigLatinizer.do(params["user_phrase"])
+
+        erb :piglatinizer
+    end
 end
